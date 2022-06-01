e51cfce74e3b63a891a53c1b0ae7e9fcb8341250
i-naeem <inaeem@protonmail.com>
2022-06-02 00:19:43 +0500

    :sparkles: make it work like a script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import spacy
 from heapq import nlargest
-from helpers import logUsage
+from helpers import logUsage, getTextFromFile, saveTextToFile
 from string import punctuation
 from spacy.lang.en.stop_words import STOP_WORDS
 
@@ -39,11 +39,18 @@ def summarize(text, per):
 
 args = sys.argv
 
-if len(args) < 2:
+
+if len(args) >= 2:
+    input_file = args[1]
+    text = getTextFromFile(input_file)
+    summary = summarize(text, 0.5)
+
+    if(len(args) > 2):
+        output_file = args[2]
+        saveTextToFile(summary, output_file)
+    else:
+        print(summary)
+
+else:
     logUsage()
     exit(1)
-
-
-s = summarize(src_text, 0.5)
-print('TEXT: \n', src_text)
-print('Summary: \n', s)
